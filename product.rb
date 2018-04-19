44257ca8aab00027fc078ab29f617f69c67ebb81
Kiran Baktha <sundarambaktha@hotmail.com>
2018-04-18 19:06:40 -0500

    Update product.rb

--- product.rb
+++ product.rb
@@ -4,7 +4,14 @@ class Product
   attr_accessor :price
   attr_accessor :quantity
 
-  def Product.pull_from_inventory(sku:, qty:)
+  def Product.pull_from_inventory(sku:, qty:,catalog:) # We use argument to decide which catalog to use Local or Online. We did not want to add 
+    # an if loop in pull from inventory because we did not want it to depend on Local and Online Catalog. Dependency Injection Principle.
+    product_data = catalog.lookup(sku)
+    # We did not want this
+    # if ENV['TEST_MODE'] == '1':
+    #  product_data = LocalCatalog.lookup(sku)
+    # else
+    #  product_data = OnlineCatalog.lookup(sku)
     p = Product.new
     p.sku = sku
     if sku == '123'
